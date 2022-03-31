86d42873c948fbad1bd64f05915aa673c63de856
Kati Raavik <kati.raavik@tptlive.ee>
2022-03-31 20:36:42 +0300

    add scss, twig, surge, purgecss

---
+++ readme.md
@@ -0,0 +1,13 @@
+# usage
+
+run dev server
+
+`npm run start`
+
+dev build
+
+`npm run dev`
+
+production build
+
+`npm run build`
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 const path = require('path');
+const glob = require('glob');
+/* // make array with pages names
+let pages = ['cat1', 'cat2', 'cat3'];
+// map a new HtmlWebpackPlugin to each of those names outputing array
+let htmlPlugins = pages.map( page=>{
+  return new HtmlWebpackPlugin({
+    filename: page + '.html',
+    template: './src/index.html'
+  });
+});
+ */
+
+const PATHS = {
+  src: path.join(__dirname, 'src')
+}
 
 module.exports = {
  // mode: 'development',  
@@ -14,6 +31,41 @@ module.exports = {
     },
     compress: true,
     port: 9000,
+    open: true,
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, //i - case insensitive, $ lõpp, kontrollib kas on css lõpuga faili
+        use: ["style-loader","css-loader"], // kui on siis kasutab seda loaderit
+      },
+      {
+        test: /\.s[ac]ss$/i, //i - case insensitive, $ lõpp, kontrollib kas on scss lõpuga faili
+        use: [MiniCssExtractPlugin.loader,"css-loader","sass-loader"], // kui on siis kasutab neid loadereid, järjekord on tähtis
+      },
+      {
+        test: /\.twig$/i, 
+        use: "twig-loader", 
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Home page',
+      template: './src/index.twig',
+      templateParameters: {
+        title: 'hello'
+      }
+    }),
+    new HtmlWebpackPlugin({
+      title: 'About us',
+      filename: 'about.html',
+      template: './src/about.twig'
+    }),
+    // ...htmlPlugins, //spread syntax, operation spreading array into existing array
+    new MiniCssExtractPlugin(),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
+    }),
+  ],
 };
